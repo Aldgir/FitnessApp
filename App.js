74eec94c42f4d3773900e81471763f23e604fe51
Aldgir <73293370+Aldgir@users.noreply.github.com>
2021-04-15 13:39:39 +0100

    Main commit
    upload project to github

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Button} from 'react-native-elements'
 export default function App() {
+  console.log('Hello World!!');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to the FItnessApp...</Text>
+      <Button> title='My fitness button'  icon= {{name:'code'}}</Button>
       <StatusBar style="auto" />
     </View>
   );
